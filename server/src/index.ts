8fa1fe8b555fec06dcfd6d23442c717be5a1ff82
yangguanghe <1649063797@qq.com>
2021-11-17 23:13:49 +0800

    movie实体类

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,11 @@
-let a:string = "yang"
-console.log(a)
+import { validate } from "class-validator";
+import { Movie } from "./entities/Movie";
+const m = new Movie();
+m.name = "第一炉香"
+m.types = ['爱情']
+m.areas = ["中国大陆"]
+m.isClassic = true
+m.timeLong = 120
+validate(m).then(error => {
+  console.log(error)
+})
